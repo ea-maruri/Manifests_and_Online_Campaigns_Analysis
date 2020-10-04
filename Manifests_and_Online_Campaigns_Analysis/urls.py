f5172db2af0d117f91ca50293be920dea04533f5
ea-maruri <64037074+ea-maruri@users.noreply.github.com>
2020-10-04 16:33:31 -0500

    add configurator view

--- Manifests_and_Online_Campaigns_Analysis/urls.py
+++ Manifests_and_Online_Campaigns_Analysis/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Manifests_and_Online_Campaigns_Analysis.views import home
+from Manifests_and_Online_Campaigns_Analysis.views import home, configurator
 
 
 urlpatterns = [
   path('admin/', admin.site.urls),
-  path('home/', home)
+  path('home/', home),
+  path('configurator/', configurator)
 ]
 
--- Manifests_and_Online_Campaigns_Analysis/views.py
+++ Manifests_and_Online_Campaigns_Analysis/views.py
@@ -5,3 +5,9 @@ def home(request):
   """Renders the home page"""
 
   return render(request, "home.html")
+
+
+def configurator(request):
+  """Renders the request page"""
+
+  return render(request, "configurator.html")
