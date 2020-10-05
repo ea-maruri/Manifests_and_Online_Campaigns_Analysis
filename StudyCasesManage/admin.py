f3ae542c5540a4678adcb3005a8f8d688bdbcd11
ea-maruri <64037074+ea-maruri@users.noreply.github.com>
2020-10-05 18:04:41 -0500

    adding Panel

--- StudyCasesManage/admin.py
+++ StudyCasesManage/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from StudyCasesManage.models import Campaign, Candidate, Manifest, SocialMediaAccount, TimeLine, Post
+
+admin.site.register(Campaign)
+admin.site.register(Candidate)
+admin.site.register(Manifest)
+admin.site.register(SocialMediaAccount)
+admin.site.register(TimeLine)
+admin.site.register(Post)
--- StudyCasesManage/models.py
+++ StudyCasesManage/models.py
@@ -1,3 +1,4 @@
+from os import name
 from django.db import models
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models.deletion import PROTECT
@@ -10,6 +11,10 @@ class Campaign(models.Model):
   end_date = models.DateField()
   description = models.CharField(max_length=50)
 
+  def __str__(self):
+    return "Campaign\n\tname: %s, start_date: %s, end_date: %s, description: %s." % (self.name, self.start_date, self.end_date, self.description)
+
+
 class Candidate(models.Model):
   campaign_id = models.ForeignKey(Campaign, on_delete=models.PROTECT)
   name = models.CharField(max_length=20)
@@ -17,12 +22,21 @@ class Candidate(models.Model):
   type = models.CharField(max_length=20)  # candidate of what?
   party = models.CharField(max_length=20)
 
+  def __str__(self):
+    return "Candidate\n\tname: %s, lastname: %s, campaign, %s, type: %s, party: %s" %(self.name, self.lastname, self.campaign_id, self.type, self.party)
+
+
 class Manifest(models.Model):
   candidate_id = models.ForeignKey(Candidate, on_delete=models.CASCADE)
   collect_date = models.DateField()
   release_date = models.DateField()
   provider = models.CharField(max_length=40)
   type = models.CharField(max_length=10)
+  # Maybe a path for the document location
+
+  def __str__(self):
+    return "Manifest\n\tcandidate: %s, collect_date: %s, release_date: %s, provider: %s, type: %s" %(self.candidate_id, self.collect_date, self.release_date, self.provider, self.type)
+
 
 class SocialMediaAccount(models.Model):
   candidate_id = models.ForeignKey(Candidate, on_delete=models.PROTECT)
@@ -32,11 +46,18 @@ class SocialMediaAccount(models.Model):
   followers = models.JSONField(encoder=DjangoJSONEncoder)
   mentions = models.JSONField(encoder=DjangoJSONEncoder)
 
+  def __str__(self):
+    return "Social Media Account\n\tcandidate: %s, screen_name: %s, creation_date: %s, description: %s" %(self.candidate_id, self.screen_name, self.created_date, self.description)
+
 class TimeLine(models.Model):
   social_media_id = models.ForeignKey(SocialMediaAccount, on_delete=models.CASCADE)
   # maybe a DateRangeField()
   collect_date = models.DateField()
   end_date = models.DateField()
+
+  def __str__(self):
+    return "Timeline\n\tsocial_medio: %s, collect_date: %s, end_date: %s" %(self.social_media_id, self.collect_date, self.end_date)
+
 
 class Post(models.Model):
   timeline_id = models.ForeignKey(TimeLine, on_delete=models.CASCADE)
@@ -49,4 +70,6 @@ class Post(models.Model):
   post_text = models.CharField(max_length=100)
   post = models.JSONField(encoder=DjangoJSONEncoder)
 
+  def __str__(self):
+    return "Post\n\tparent: %s, post_date: %s, text: %s" %(self.parent_id, self.post_date, self.post_text)
 
