b6f99420bf76457f918929b041653543529f6799
ea-maruri <64037074+ea-maruri@users.noreply.github.com>
2020-11-17 17:03:13 -0500

    update settings (staticfiles_dirs)

--- Manifests_and_Online_Campaigns_Analysis/settings.py
+++ Manifests_and_Online_Campaigns_Analysis/settings.py
@@ -133,7 +133,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+if not DEBUG:
+    STATIC_ROOT = ''
+    # STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [ 
+    BASE_DIR / 'static',
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
